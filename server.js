a836597ce3704036b7bdf0e90e9fcd5b57de9489
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-05-10 12:20:20 +0200

    added my own data, created mongoose model, seeding the db, first route

--- server.js
+++ server.js
@@ -9,10 +9,30 @@ import mongoose from "mongoose";
 // import goldenGlobesData from "./data/golden-globes.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
+import animalsData from "./data/animals-data.json";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/animals";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const Animal = mongoose.model("Animal", {
+  animalID: Number,
+  name: String,
+  animal: String,
+  color: String,
+  isFurry: Boolean
+})
+
+if (process.env.RESET_DB) {
+  const seedDatabase = async () => {
+    await Animal.deleteMany({})
+
+    animalsData.forEach((animalItem) => {
+      new Animal(animalItem).save()
+    })
+  }
+  seedDatabase()
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -26,9 +46,23 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello Camilla!");
 });
 
+// Route to get all animals from my data set
+app.get("/collection", async (req, res) => {
+  const collectionOfAnimals = await Animal.find()
+  res.json(collectionOfAnimals)
+})
+
+// Endpoint to search for an animal by species:
+
+// Endpoint (?) to show all animals that are furry and those that are not:
+
+// Endpoint to search for a specific color:
+
+// Endpoint to search for an animal name:
+  
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
